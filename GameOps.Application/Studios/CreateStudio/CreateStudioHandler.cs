47e03f8cc75c9a24b2a7c7c30b3d2658ae425c36
Javier Matejko <javiermatejko-externo@tgacompany.com>
2026-01-24 14:46:27 -0300

    chore(project): added CreateStudioHandler to CreateStudio folder

---
+++ GameOps.Application/Studios/CreateStudio/CreateStudioHandler.cs
@@ -0,0 +1,29 @@
+﻿using GameOps.Application.Abstractions;
+using GamesOps.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOps.Application.Studios.CreateStudio
+{
+    public class CreateStudioHandler
+    {
+        private readonly IStudioRepository _studioRepository;
+
+        public CreateStudioHandler(IStudioRepository studioRepository)
+        {
+            _studioRepository = studioRepository;
+        }
+
+        public async Task<Guid> Handle(CreateStudioCommand command)
+        {
+            var studio = new Studio(command.Name);
+
+            await _studioRepository.AddAsync(studio);
+
+            return studio.Id;
+        }
+    }
+}
