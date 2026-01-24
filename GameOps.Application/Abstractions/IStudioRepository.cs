5c905d1811937968188fb25d95dc47a9c6028e90
Javier Matejko <javiermatejko-externo@tgacompany.com>
2026-01-24 14:45:59 -0300

    chore(project): added IStudioRepository to Abastractions folder

---
+++ GameOps.Application/Abstractions/IStudioRepository.cs
@@ -0,0 +1,10 @@
+﻿using GamesOps.Domain.Entities;
+
+namespace GameOps.Application.Abstractions
+{
+    public interface IStudioRepository
+    {
+        Task AddAsync(Studio studio);
+        Task<Studio?> GetByIdAsync(Guid id);
+    }
+}
