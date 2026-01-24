8fed7f0840a5794f3eee45972c17e2ba6f09bdb3
Javier Matejko <javiermatejko-externo@tgacompany.com>
2026-01-24 14:00:27 -0300

    chore(project): added Studio entity to project

---
+++ GamesOps.Domain/Entities/Studio.cs
@@ -0,0 +1,42 @@
+﻿using GamesOps.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamesOps.Domain.Entities
+{
+    public class Studio
+    {
+        private Guid id;
+        private string name;
+        private DateTime createdAt;
+
+        public Guid Id { get => id; }
+        public string Name { get => name; }
+        public DateTime CreatedAt { get => createdAt; }
+
+        public Studio(string studioName)
+        {
+            if(string.IsNullOrWhiteSpace(studioName))
+            {
+                throw new DomainException($"Studio name cannot be empty");
+            }
+
+            id = Guid.NewGuid();
+            name = studioName.Trim();
+            createdAt = DateTime.UtcNow;
+        }
+
+        public void Rename(string newStudioName)
+        {
+            if (string.IsNullOrWhiteSpace(newStudioName))
+            {
+                throw new DomainException($"Studio name cannot be empty");
+            }
+
+            name = newStudioName.Trim();
+        }
+    }
+}
