39c80964df174ebd01cb9a24cb0c64b968fcc6bc
Javier Matejko <javiermatejko-externo@tgacompany.com>
2026-01-24 18:06:05 -0300

    feat(project): added studio configuration script

---
+++ GameOps.Infrastructure/Persistence/Configurations/StudioConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using GamesOps.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GameOps.Infrastructure.Persistence.Configurations
+{
+    public class StudioConfiguration : IEntityTypeConfiguration<Studio>
+    {
+        public void Configure(EntityTypeBuilder<Studio> builder)
+        {
+            builder.ToTable("Studios");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(x => x.CreatedAt)
+                .IsRequired();
+        }
+    }
+}
