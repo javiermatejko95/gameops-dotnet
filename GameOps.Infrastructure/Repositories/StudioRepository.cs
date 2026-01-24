deb2583ea5f3f4c244c0a0bdfbbb412172b8c20e
Javier Matejko <javiermatejko-externo@tgacompany.com>
2026-01-24 18:06:27 -0300

    feat(project): added studio respository script

---
+++ GameOps.Infrastructure/Repositories/StudioRepository.cs
@@ -0,0 +1,35 @@
+﻿using GameOps.Application.Abstractions;
+using GameOps.Infrastructure.Persistence;
+using GamesOps.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameOps.Infrastructure.Repositories
+{
+    public class StudioRepository : IStudioRepository
+    {
+        private readonly GameOpsDbContext _context;
+
+        public StudioRepository(GameOpsDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Studio studio)
+        {
+            await _context.Studios.AddAsync(studio);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Studio?> GetByIdAsync(Guid id)
+        {
+            return await _context.Studios
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<bool> ExistsByNameAsync(string name)
+        {
+            return await _context.Studios
+                .AnyAsync(s => s.Name == name);
+        }
+    }
+}
