9a1d22d5d63344ea3a3c3824cc0231a2b048cf82
Javier Matejko <javiermatejko-externo@tgacompany.com>
2026-01-24 18:06:18 -0300

    feat(project): added GameOpsDbContext script

---
+++ GameOps.Infrastructure/Persistence/GameOpsDbContext.cs
@@ -0,0 +1,22 @@
+﻿using GamesOps.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameOps.Infrastructure.Persistence
+{
+    public class GameOpsDbContext : DbContext
+    {
+        public DbSet<Studio> Studios => Set<Studio>();
+
+        public GameOpsDbContext(DbContextOptions<GameOpsDbContext> options)
+        : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(GameOpsDbContext).Assembly);
+        }
+    }
+}
